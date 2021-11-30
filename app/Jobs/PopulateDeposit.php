555583697a50987216c5eaf8834d2acada645833
root <root@ip-172-31-42-130.ap-southeast-1.compute.internal>
2021-11-30 04:32:37 +0000

    dari server

--- app/Jobs/PopulateDeposit.php
+++ app/Jobs/PopulateDeposit.php
@@ -41,9 +41,13 @@ class PopulateDeposit implements ShouldQueue
         ->select('depo.id as id', 'depo.scan_id as scan_id', 'depo.bank as bank', 'depo.name as name', 'depo.rekening as rekening', 'category_type', 'username', 'jumlah', 'status', 'useradmin', 'date', 'approved_at', 'depo.lastUpdate as lastUpdate', 'remark', 'warning_status', 'img_url', 'depo.rate as rate', 'rekening.bank as toBank', 'rekening.name as toName', 'rekening.rekening as toNumber')
         ->where('depo.scan_id', $this->scan_id)
         ->join('rekening', 'depo.rekening_tujuan', '=', 'rekening.id')
-        ->orderBy('depo.id')
-        ->chunk(200, function ($deposits) {
+        ->orderBy('depo.id', 'DESC')
+        ->chunk(2000, function ($deposits) {
             foreach($deposits as $depo){
+		if(DB::table('deposits_migrations')->where('transaction_id', $depo->id)->exists()){
+			Log::debug('ada '.$depo->id); 
+		}
+		else{
                 $member_url = "http://172.31.39.201";
                 $member_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJteS1wcm9qZWN0IiwiZXhwIjoxNTA5NjUwODAxLCJpYXQiOjE1MDk2NTQ0MDF9.F4iKO0R0wvHkpCcQoyrYttdGxE5FLAgDhbJOSLEHIBPsbL2WkLxXB9IGbDESn9rE7oxn89PJFRtcLn7kJwvdQkQcsPxn2RQorvDAnvAi1w3k8gpxYWo2DYJlnsi7mxXDqSUCNm1UCLRCW68ssYJxYLSg7B1xGMgDADGyYPaIx1EdN4dDbh-WeDyLLa7a8iWVBXdbmy1H3fEuiAyxiZpk2ll7DcQ6ryyMrU2XadwEr9PDqbLe1SrlaJsQbFi8RIdlQJSo_DZGOoAlA5bYTDYXb-skm7qvoaH5uMtOUb0rjijYuuxhNZvZDaBerEaxgmmlO0nQgtn12KVKjmKlisG79Q";
 
@@ -63,6 +67,8 @@ class PopulateDeposit implements ShouldQueue
                     ])->json();
                 }catch(Exception $e){
                     Log::warning($e);
+			Log::warning($username);
+			continue;
                 }
 
                 if($memberSimple["success"] == true){
@@ -73,7 +79,227 @@ class PopulateDeposit implements ShouldQueue
                     $warning = $memberSimple["data"]["warningStatus"];
                 }
 
-                try{
+if($depo->toBank == "CIMB" || $depo->toBank == "cimbniaga"){
+$depo->toBank = "cimb";
+}
+
+if($depo->toBank == "otherbank"){
+$depo->toBank = "banklain";
+}
+
+if($depo->bank == "otherbank"){
+$depo->bank = "banklain";
+}
+
+if($depo->bank == "CIMB" || $depo->bank == "cimbniaga"){
+$depo->bank = "cimb";
+}
+
+if($depo->bank == "BCA" || $depo->bank == "qr-bankbca"){
+$depo->bank = "bca";
+}
+
+if($depo->bank == "BNI"){
+$depo->bank = "bni";
+}
+
+if($depo->bank == "BRI"){
+$depo->bank = "bri";
+}
+
+if($depo->bank == "Danamon" || $depo->bank == "bankdanamon"){
+$depo->bank = "danamon";
+}
+
+if($depo->bank == "Mandiri"){
+$depo->bank = "mandiri";
+}
+
+if($depo->bank == "mandiri_online" || $depo->bank == "mandiri2"){
+$depo->bank = "mandiri";
+}
+
+if($depo->toBank == "BCA" || $depo->toBank == "bca2" || $depo->toBank == "bca3" || $depo->toBank == "qr-bankbca"){
+$depo->toBank = "bca";
+}
+
+if($depo->toBank == "BNI"){
+$depo->toBank = "bni";
+}
+
+if($depo->toBank == "BRI" || $depo->toBank == "bri2"){
+$depo->toBank = "bri";
+}
+
+if($depo->toBank == "Danamon" || $depo->toBank == "bankdanamon"){
+$depo->toBank = "danamon";
+}
+
+if($depo->toBank == "dana1" || $depo->toBank == "dana2" || $depo->toBank == "dana3" || $depo->toBank == "dana4" || $depo->toBank == "dana5" || $depo->toBank == "dana6" || $depo->toBank == "dana7" || $depo->toBank == "dana8" || $depo->toBank == "dana9" || $depo->toBank == "dana10" || $depo->toBank == "dana11" || $depo->toBank == "dana12" || $depo->toBank == "dana13" || $depo->toBank == "dana14" || $depo->toBank == "dana15" || $depo->toBank == "dana16" || $depo->toBank == "dana17" || $depo->toBank == "dana18" || $depo->toBank == "dana19" || $depo->toBank == "dana20" || $depo->toBank == "dana21" || $depo->toBank == "dana22" || $depo->toBank == "dana23" || $depo->toBank == "qr-dana"){
+    $depo->toBank = "dana";
+}
+
+if($depo->toBank == "ovo2" || $depo->toBank == "ovo3" || $depo->toBank == "ovo4" || $depo->toBank == "qr-ovo"){
+    $depo->toBank = "ovo";
+}
+
+if($depo->bank == "ovo2" || $depo->bank == "ovo3" || $depo->bank == "ovo4"){
+$depo->bank = "ovo";
+}
+
+if($depo->toBank == "Mandiri" || $depo->toBank == "mandiri_online" || $depo->toBank == "mandiri2"){
+$depo->toBank = "mandiri";
+}
+
+if($depo->toBank == "qr-dana"){
+$depo->toBank = "dana";
+}
+
+if($depo->toBank == "qr-gopay"){
+$depo->toBank = "gopay";
+}
+
+if($depo->toBank == "qr-linkaja"){
+$depo->toBank = "linkaja";
+}
+
+if($depo->toBank == "qr-sakuku"){
+$depo->toBank = "sakuku";
+}
+
+if($depo->toBank == "qr-shopeepay"){
+$depo->toBank = "shopeepay";
+}
+
+if($depo->toBank == "jeniusbtpn"){
+$depo->toBank = "jenius";
+}
+
+if($depo->bank == "jeniusbtpn"){
+$depo->bank = "jenius";
+}
+
+if($depo->toBank == "bankpapua"){
+$depo->toBank = "papua";
+}
+
+$xl = ['xlatauaxis', 
+'xlatauaxis11', 
+'xlatauaxis12', 
+'xlatauaxis13', 
+'xlatauaxis14', 
+'xlatauaxis15', 
+'xlatauaxis16', 
+'xlatauaxis17', 
+'xlatauaxis18', 
+'xlatauaxis19', 
+'xlatauaxis20',
+'xl/axis11',
+'xl/axis12',
+'xl/axis13',
+'xl/axis14',
+'xl/axis15',
+'xl/axis16',
+'xl/axis17',
+'xl/axis18',
+'xl/axis20',
+'axis',
+'axis/xl',
+'xl/axis',
+'xl-all-operator'];
+
+if(in_array($depo->bank, $xl)){
+    $depo->bank = 'xl';
+}
+
+if(in_array($depo->toBank, $xl)){
+    $depo->toBank = 'xl';
+}
+
+$telkomsel = ['telkomsel',
+'telkomsel1',
+'telkomsel2',
+'telkomsel3',
+'telkomsel4',
+'telkomsel40',
+'telkomsel41',
+'telkomsel42',
+'telkomsel44',
+'telkomsel45',
+'telkomsel46',
+'telkomsel47',
+'telkomsel48',
+'telkomsel49',
+'telkomsel5',
+'telkomsel50',
+'telkomsel51',
+'telkomsel52',
+'telkomsel53',
+'telkomsel54',
+'telkomsel55',
+'telkomsel56',
+'telkomsel57',
+'telkomsel58',
+'telkomsel59',
+'telkomsel60',
+'telkomsel61',
+'telkomsel62',
+'telkomsel63',
+'telkomsel64',
+'telkomsel65',
+'telkomsel66',
+'telkomsel67',
+'telkomsel68',
+'telkomsel70',
+'telkomsel71',
+'telkomsel72',
+'telkomsel73',
+'telkomsel74'];
+
+if(in_array($depo->bank, $telkomsel)){
+    $depo->bank = 'telkomsel';
+}
+
+if(in_array($depo->toBank, $telkomsel)){
+    $depo->toBank = 'telkomsel';
+}
+
+$categories = [
+        'bca' => 'bank',
+        'bri' => 'bank',
+        'bni' => 'bank',
+        'mandiri' => 'bank',
+        'cimb' => 'bank',
+	'banklain' => 'bank',
+        'danamon' => 'bank',
+	'btpn' => 'bank',
+	'maybank' => 'bank',
+	'permata' => 'bank',
+	'jago' => 'bank',
+	'mega' => 'bank',
+	'bsi' => 'bank',
+	'btn' => 'bank',
+	'ocbc' => 'bank',
+	'panin' => 'bank',
+	'papua' => 'bank',
+	'shopeepay' => 'epayment',
+	'sakuku' => 'epayment',
+        'dana' => 'epayment',
+        'ovo' => 'epayment',
+	'gopay' => 'epayment',
+	'qris' => 'epayment',
+	'linkaja' => 'epayment',
+	'jenius' => 'epayment',
+	'doku' => 'epayment',
+        'telkomsel' => 'phonecredit',
+        'xl' => 'phonecredit',
+	'tri' => 'phonecredit'
+];
+
+$categoryFrom = $categories[$depo->bank];
+$categoryTo = $categories[$depo->toBank];
+
+                /*try{
                     $categoriesFrom = Http::withToken($payment_token)
                     ->timeout(5)
                     ->retry(3, 100)
@@ -89,12 +315,20 @@ class PopulateDeposit implements ShouldQueue
                     ])->json();
                 }catch(Exception $e){
                     Log::warning($e);
+			Log::warning($depo->bank);
+			Log::warning($depo->toBank);
+
                 }
+
+if(!isset($categoriesFrom["data"]["categories"]) || !isset($categoriesTo["data"]["categories"])){
+Log::warning($depo->bank);
+Log::warning($depo->toBank);
+}
 
                 if($categoriesFrom["success"] == true && $categoriesTo["success"] == true){
                     $categoryFrom = $categoriesFrom["data"]["categories"];
                     $categoryTo = $categoriesTo["data"]["categories"];
-                }
+                } */
 
                 $amountSubmitted = ($depo->jumlah / 1000) / ($depo->rate / 100);
                 $amountProcessed = $currency == "IDR" ? $depo->jumlah / 1000 : $depo->jumlah;
@@ -154,6 +388,7 @@ class PopulateDeposit implements ShouldQueue
 
                 DB::table('deposits_migrations')->insertOrIgnore($inserted);
             }
+	}
         });
     }
 }
--- app/Jobs/PopulateWithdraw.php
+++ app/Jobs/PopulateWithdraw.php
@@ -41,7 +41,7 @@ class PopulateWithdraw implements ShouldQueue
         DB::connection('v1')->table('withdraw')
         ->select('withdraw.id as id', 'withdraw.scan_id as scan_id', 'withdraw.bank as bank', 'withdraw.name as name', 'withdraw.rekening as rekening', 'category_type', 'username', 'jumlah', 'status', 'useradmin', 'date', 'approved_at', 'withdraw.lastUpdate as lastUpdate', 'remark', 'warning_status')
         ->where('withdraw.scan_id', $this->scan_id)
-        ->orderBy('withdraw.id')
+        ->orderBy('withdraw.id', 'DESC')
         ->chunk(500, function ($withdraws) {
             foreach($withdraws as $wd){
                 $member_url = "http://172.31.39.201";
@@ -73,7 +73,80 @@ class PopulateWithdraw implements ShouldQueue
                     $warning = $memberSimple["data"]["warningStatus"];
                 }
 
-                try{
+if($wd->bank == "BCA"){
+$wd->bank = "bca";
+}
+
+if($wd->bank == "otherbank"){
+$wd->bank = "banklain";
+}
+
+if($wd->bank == "BNI"){
+$wd->bank = "bni";
+}
+
+if($wd->bank == "BRI"){
+$wd->bank = "bri";
+}
+
+if($wd->bank == "Danamon"){
+$wd->bank = "danamon";
+}
+
+if($wd->bank == "ovo3" || $wd->bank == "ovo4"){
+$wd->bank = "ovo";
+}
+
+if($wd->bank == "Mandiri" || $wd->bank == "mandiri_online" || $wd->bank == "mandiri2"){
+$wd->bank = "mandiri";
+}
+
+if($wd->bank == "cimbniaga" || $wd->bank == "cimbniaga"){
+$wd->bank = "cimb";
+}
+
+if($wd->bank == "jeniusbtpn"){
+$wd->bank = "jenius";
+}
+
+if($wd->bank == "bankdanamon"){
+$wd->bank = "danamon";
+}
+
+$categories = [
+        'bca' => 'bank',
+        'bri' => 'bank',
+        'bni' => 'bank',
+        'mandiri' => 'bank',
+        'cimb' => 'bank',
+        'banklain' => 'bank',
+        'danamon' => 'bank',
+        'btpn' => 'bank',
+        'maybank' => 'bank',
+        'permata' => 'bank',
+        'jago' => 'bank',
+        'mega' => 'bank',
+        'bsi' => 'bank',
+        'btn' => 'bank',
+        'ocbc' => 'bank',
+        'panin' => 'bank',
+        'shopeepay' => 'epayment',
+        'sakuku' => 'epayment',
+        'dana' => 'epayment',
+        'ovo' => 'epayment',
+        'gopay' => 'epayment',
+        'qris' => 'epayment',
+        'linkaja' => 'epayment',
+        'jenius' => 'epayment',
+        'doku' => 'epayment',
+        'telkomsel' => 'phonecredit',
+        'xl' => 'phonecredit',
+        'tri' => 'phonecredit'
+];
+
+$categoryTo = $categories[$wd->bank];
+
+                /*try{
                     $categoriesTo = Http::withToken($payment_token)
                     ->timeout(5)
                     ->retry(3, 100)
@@ -84,9 +157,14 @@ class PopulateWithdraw implements ShouldQueue
                     Log::warning($e);
                 }
 
+if(!isset($categoriesTo["data"]["categories"])){
+Log::warning($wd->bank);
+}
+
+
                 if($categoriesTo["success"] == true){
                     $categoryTo = $categoriesTo["data"]["categories"];
-                }
+                }*/
 
                 $amountSubmitted = $currency == "IDR" ? $wd->jumlah / 1000 : $wd->jumlah;
                 $amountProcessed = $currency == "IDR" ? $wd->jumlah / 1000 : $wd->jumlah;
